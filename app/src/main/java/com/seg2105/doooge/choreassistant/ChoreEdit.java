b15c4edb2a4415bedb315a35aa743c768ade9154
Dustin Holloway <dholl086@uottawa.ca>
2017-11-22 16:57:34 -0500

    Create chore edit ui, ability to edit text and select important information

---
+++ app/src/main/java/com/seg2105/doooge/choreassistant/ChoreEdit.java
@@ -0,0 +1,111 @@
+package com.seg2105.doooge.choreassistant;
+
+/**
+ * Created by dustin on 11/22/17.
+ */
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import java.util.Calendar;
+
+
+public class ChoreEdit  extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    //public void btnSelectDate(View view) {
+    public void textDate_OnClick(View view) {
+        datePick();
+    }
+
+
+    //public void btnTime(View view){
+    public void textTimne_OnClick(View view) {
+        timePick();
+    }
+
+    /**
+     * Set the text of the textDate field.
+     *
+     * @param year  calendar year
+     * @param month calendar month
+     * @param day   calendar day
+     */
+    private void setDate(int year, int month, int day) {
+        TextView textDate = (TextView) findViewById(R.id.textDate);
+        textDate.setText(day + "/" + month + "/" + year);
+    }
+
+    /**
+     * Set the text of the textTime field.
+     *
+     * @param hour   time in hours
+     * @param minute time in minutes
+     */
+    private void setTime(int hour, int minute) {
+        TextView textTime = (TextView) findViewById(R.id.textTime);
+        textTime.setText(hour + ":" + minute);
+    }
+
+
+    //https://developer.android.com/reference/android/app/TimePickerDialog.html
+    private void timePick() {
+
+        Calendar cal = Calendar.getInstance();
+        int hour = cal.get(Calendar.HOUR);
+        int minute = cal.get(Calendar.MINUTE);
+
+        //Time time = new Time();
+        //int hour = time.hour;
+        //int minute = time.minute;
+
+        TimePickerDialog temp = new TimePickerDialog(this, timeListen, hour, minute, false);
+        temp.show();
+    }
+
+    //https://developer.android.com/reference/android/app/TimePickerDialog.OnTimeSetListener.html
+    private TimePickerDialog.OnTimeSetListener timeListen = new TimePickerDialog.OnTimeSetListener() {
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            setTime(hourOfDay, minute);
+        }
+    };
+
+
+    //https://developer.android.com/reference/android/app/DatePickerDialog.html
+    private void datePick() {
+
+        //year, month, and day have to be set before calling or the date is very very very very very wrong
+        //https://developer.android.com/reference/java/util/Calendar.html
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+
+        DatePickerDialog temp = new DatePickerDialog(this, tempListen, year, month, day);
+        temp.show();
+        //temp.setOnDateSetListener(tempListen);
+    }
+
+    //https://developer.android.com/reference/android/app/DatePickerDialog.OnDateSetListener.html
+    private DatePickerDialog.OnDateSetListener tempListen = new DatePickerDialog.OnDateSetListener() {
+
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            //From article: the selected month (0-11 for compatibility with MONTH), so add 1...
+            setDate(year, month + 1, dayOfMonth);
+        }
+
+    };
+}
