a89df63be8f97dcdd51f1a899ba0677e54d09440
Dustin Holloway <dholl086@uottawa.ca>
2017-11-22 19:33:24 -0500

    Main screen UI for all users

--- app/src/main/java/com/seg2105/doooge/choreassistant/ChoreEdit.java
+++ app/src/main/java/com/seg2105/doooge/choreassistant/ChoreEdit.java
@@ -19,7 +19,7 @@ public class ChoreEdit  extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.chore_edit);
 
     }
 
