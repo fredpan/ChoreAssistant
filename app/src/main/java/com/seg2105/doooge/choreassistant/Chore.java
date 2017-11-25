07640ba79c14f2d02bbd2a498e0ae9ef4a5589be
Dustin Holloway <dholl086@uottawa.ca>
2017-11-25 18:39:05 -0500

    add chore edit ui...

---
+++ app/src/main/java/com/seg2105/doooge/choreassistant/Chore.java
@@ -0,0 +1,14 @@
+package com.seg2105.doooge.choreassistant;
+
+/**
+ * Created by dustin on 11/25/17.
+ */
+
+public class Chore {
+
+    private String description;
+    private int day;
+    private int month;
+
+
+}
